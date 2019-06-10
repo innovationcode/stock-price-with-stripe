cc80403e33880a107f7f2d130babaf4bac40f7a4
lajawanti <myemail>
2019-06-10 10:58:10 -0400

    some route changes

--- migrations/versions/5e64412be6d4_users_table.py
+++ migrations/versions/5e64412be6d4_users_table.py
@@ -23,7 +23,6 @@ def upgrade():
     sa.Column('username', sa.String(length=64), nullable=True),
     sa.Column('email', sa.String(length=120), nullable=True),
     sa.Column('password_hash', sa.String(length=128), nullable=True),
-    #sa.Column('premium_user', sa.Boolean(), default=False), #########################################
 
     sa.PrimaryKeyConstraint('id')
     )
